eddfe18ddd5940450086d8e7de0abf3c4e2c38a5
Peter Skeide <peterskeide@gmail.com>
2014-05-12 11:52:05 +0200

    Use hash instead of interpolated XML

--- lib/24_seven_office/services/authentication.rb
+++ lib/24_seven_office/services/authentication.rb
@@ -7,23 +7,16 @@ module TwentyFourSevenOffice
 
       operations :login
 
-      Request = Struct.new(:username, :password, :application_id) do
-        def to_s
-          <<-XML
-          <tns:credential>
-            <tns:Type>Community</tns:Type>
-            <tns:Username>#{username}</tns:Username>
-            <tns:Password>#{password}</tns:Password>
-            <tns:IdentityId>#{application_id}</tns:IdentityId>
-          </tns:credential>
-          XML
-        end
-      end
-
       def self.login(username, password, application_id)
-        super message: Request.new(username, password, application_id)
+        super message: { 
+          credential: {
+            type: "Community",
+            username: username,
+            password: password,
+            identity_id: application_id
+          }
+        }
       end
-
     end
   end
 end
