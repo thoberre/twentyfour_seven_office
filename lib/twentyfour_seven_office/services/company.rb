a28b065a514901646069a3e4dbfba483aad59460
Peter Skeide <peterskeide@gmail.com>
2014-05-19 09:33:34 +0200

    Add a method for retrieving all Companies

--- lib/twentyfour_seven_office/services/company.rb
+++ lib/twentyfour_seven_office/services/company.rb
@@ -7,9 +7,19 @@ module TwentyfourSevenOffice
                     input_data_types: { search_params: CompanySearchParameters, return_properties: Array }
 
       def where(query = {})
-        return_params = ::TwentyfourSevenOffice::DataTypes::Company.new.attributes.keys
-        return_params = return_params.map { |attr| camelcase(attr.to_s) }
-        get_companies(search_params: query, return_properties: return_params)
+        get_companies(search_params: query, return_properties: return_props)
+      end
+
+      def all
+        search_params = { changed_after: DateTime.new(1970, 1, 1) }
+        get_companies(search_params: changed_after, return_properties: return_props)
+      end
+
+      private
+
+      def return_props
+        rp = ::TwentyfourSevenOffice::DataTypes::Company.new.attributes.keys
+        rp.map { |attr| camelcase(attr.to_s) }
       end
     end
   end
