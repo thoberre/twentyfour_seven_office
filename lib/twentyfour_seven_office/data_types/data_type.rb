6e75d33b91393b490a1b43f54620839c3677fda6
Peter Skeide <peterskeide@gmail.com>
2014-05-22 15:27:56 +0200

    Make DataType#to_request support «array of data type» attributes

--- lib/twentyfour_seven_office/data_types/data_type.rb
+++ lib/twentyfour_seven_office/data_types/data_type.rb
@@ -6,7 +6,9 @@ module TwentyfourSevenOffice
 
       def to_request
         attrs = attributes.reject { |k, v| v.nil? }.map do |k, v|
-          if v.respond_to?(:to_request)
+          if is_array_of_data_types?(v)
+            v = v.map { |item| item.to_request }
+          elsif v.respond_to?(:to_request)
             v = v.to_request
           end
 
@@ -14,6 +16,12 @@ module TwentyfourSevenOffice
         end
 
         Hash[attrs]
+      end
+
+      private
+
+      def is_array_of_data_types?(ary)
+        ary.is_a?(Array) && ary.any? && ary.first.respond_to?(:to_request)
       end
     end
   end
---
+++ spec/data_types/data_type_spec.rb
@@ -0,0 +1,63 @@
+require "spec_helper"
+
+describe TwentyfourSevenOffice::DataTypes::DataType do
+  describe "#to_request" do
+    it "recursively converts data types to their xml compatible representations" do
+      c = TwentyfourSevenOffice::DataTypes::Company.new(
+        id: 1234,
+        name: "ACME",
+        country: "NO",
+        type: "Consumer",
+        addresses: {
+          post: {
+            street: "Portveien 2",
+            country: "Norway"
+          }
+        },
+        phone_numbers: {
+          primary: {
+            value: "11 11 11 11"
+          }
+        }
+      )
+
+      hash = c.to_request
+
+      expect(hash).to eq({
+        Id: 1234,
+        Name: "ACME",
+        Country: "NO",
+        Type: "Consumer",
+        Addresses: {
+          Post: {
+            Street: "Portveien 2",
+            Country: "Norway"
+          }
+        },
+        PhoneNumbers: {
+          Primary: {
+            Value: "11 11 11 11"
+          }
+        }
+      })
+    end
+
+    it "supports attributes for arrays of data types" do
+      io = TwentyfourSevenOffice::DataTypes::InvoiceOrder.new(
+        invoice_rows: [
+          TwentyfourSevenOffice::DataTypes::InvoiceRow.new(price: 5.0),
+          TwentyfourSevenOffice::DataTypes::InvoiceRow.new(price: 10.0),
+          TwentyfourSevenOffice::DataTypes::InvoiceRow.new(price: 15.0)
+        ]
+      )
+
+      hash = io.to_request
+
+      expect(hash).to eq({
+        InvoiceRows: [
+          { Price: 5.0 }, { Price: 10.0 }, { Price: 15.0 }
+        ]
+      })
+    end
+  end
+end
